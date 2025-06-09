69f9d74bf9c09c54b101dc4dab7ef2f111167ac2
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-06-09 19:26:27 -0300

    Create fotovoltaico.cpp

---
+++ init/fotovoltaico.cpp
@@ -0,0 +1,25 @@
+#define LED 18
+#define ldr 32
+
+int leitura_ldr = 0;
+float tensao_ldr = 0; 
+
+void setup() {
+   
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  
+  leitura_ldr = analogRead(ldr);
+  Serial.println(leitura_ldr);
+  tensao_ldr =  (3.3 *  leitura_ldr)/4096;
+  Serial.print("Tensão: ");
+  Serial.println(tensao_ldr);
+  delay(1000); 
+
+  if (leitura_ldr < 4000) digitalWrite(LED, HIGH);
+  else digitalWrite(LED, LOW);
+  delay(200);
+  }
