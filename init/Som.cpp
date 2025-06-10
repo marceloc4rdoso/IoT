814f6e076430aa8bb9b77b2376b7ecbde4c530ad
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-06-10 20:51:09 -0300

    Update Som.cpp

--- init/Som.cpp
+++ init/Som.cpp
@@ -1,9 +1,12 @@
 //Este programa faz a medição da distância entre o sensor HC-SR04 e um objeto à sua frente.
 
-#define TRIG 16
+#define TRIG 18
 #define ECHO 2
 #define BUZZER 4
 int distancia;
+int ok = 30;
+int cuidado = 10;
+
 
 void setup() {
   Serial.begin(9600);
@@ -14,23 +17,33 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-medir();
-Serial.print("Distancia em CM:");
-Serial.println(distancia);
+  medir();
+  Serial.print("Distancia em CM:");
+  Serial.println(distancia);
+  //detectar();
 }
 
 
-void medir(){
+void medir() {
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
   // lê o resultado:
   distancia = pulseIn(ECHO, HIGH);
-  distancia = distancia * 0.0343 / 2; // duração do pulso X velocidade do som no ar a 20°C (343 m/s)/ 2distance/58;
+  distancia = distancia * 0.0343 / 2;  // duração do pulso X velocidade do som no ar a 20°C (343 m/s)/ 2distance/58;
+
+
+  if (distancia > 30) {
+    Serial.println("Tudo OK");
+    digitalWrite(BUZZER, LOW);
+  }
+  else if (distancia >= 10 && distancia <= 30) {
+    Serial.println("CUIDADO!");
+    digitalWrite(BUZZER, LOW);
+  } else {
+    Serial.println("Colisão!");
+    digitalWrite(BUZZER, HIGH);
+  }
+
   delay(300);
 }
-
-
-
-
-//dis (velocidade do som * tempo)/2
